12c5bc330e997e374b16a5e647b5dc77874c6eaf
bryan <bry_lar@yahoo.com>
2024-10-01 14:34:45 +0100

    added new implementation

--- src/main/java/com/serenity/serenity/data/Patient.java
+++ src/main/java/com/serenity/serenity/data/Patient.java
@@ -18,9 +18,11 @@ private String mobile;
 @JsonProperty("mr_number")
 private String mrNumber;
 private String id;
+@JsonProperty("birth_date")
 private String dob;
 private String gender;
 private double confidence;
+@JsonProperty("external_system")
 private String source;
 
 
