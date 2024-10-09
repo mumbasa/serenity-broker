aa015c6e60c235cce9f1223562f26dabfd8f4827
bryan <bry_lar@yahoo.com>
2024-10-09 13:31:10 +0100

    new ecnounternote

--- src/main/java/com/serenity/serenity/data/his/EncounterNote.java
+++ src/main/java/com/serenity/serenity/data/his/EncounterNote.java
@@ -1,5 +1,7 @@
 package com.serenity.serenity.data.his;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,23 +15,38 @@ import lombok.ToString;
 @ToString
 @Entity
 public class EncounterNote {
-    @Id
+  @Id
    @GeneratedValue(strategy=GenerationType.SEQUENCE)
     private long id;
+
     private String uuid;
+    @JsonProperty("created_at")
     private String createdAt;
+    @JsonProperty("updated_at")
     private String updatedAt;
     private String note;
+    @JsonProperty("encounter_id")
     private String encounterId;
+    @JsonProperty("encounter_date")
     private String encounterDate;
+    @JsonProperty("patient_mr_number")
     private String patientMrNumber;
+    @JsonProperty("encounter_type")
     private String encounterType;
+    @JsonProperty("note_type")
     private String noteType;
+    @JsonProperty("is_edited")
     private boolean isEdited;
+    @JsonProperty("is_recalled")
     private boolean isRecalled;
+    @JsonProperty("is_formatted")
     private boolean isFormatted;
+    @JsonProperty("practitioner_name")
     private String practitionerName;
+    @JsonProperty("pratitioner_role_type")
     private String practitionerRoleType;
+    @JsonProperty("practitioner_id")
     private String practitionerId;
+    
     private String dataSource;
 }
