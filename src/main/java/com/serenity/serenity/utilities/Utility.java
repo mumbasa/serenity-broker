fe9dca046e98bb5054b2fae59729a90b4aa65450
Bryan Laryea <bry_lar@yahoo.com>
2024-10-17 15:53:57 +0000

    fixed serialization bug

--- src/main/java/com/serenity/serenity/utilities/Utility.java
+++ src/main/java/com/serenity/serenity/utilities/Utility.java
@@ -108,7 +108,6 @@ public class Utility {
                 + "{\"locationId\": \"b60c55f5-63dd-4ba2-9fe9-8192f57aaed2\",\"locationName\": \"Tema Primary Care\"},"
                 + "{\"locationId\": \"a79ae42b-03b7-4f5e-ac1a-cd42729c0b04\",\"locationName\": \"Takoradi Primary Care\"},"
                 + "{\"locationId\": \"29e22113-9d7b-46a6-a857-810ca3567ca7\",\"locationName\": \"Airport Main\"},"
-                + "{\"locationId\": \"25d3f170-2c76-413f-b4e0-7c09b4847b68\",\"locationName\": \"Kumasi\"},"
                 + "{\"locationId\": \"2550dc16-3f64-4cee-b808-6c13b255d159\",\"locationName\": \"Ward - Airport Main\"}"+
                 "]";
         Gson gson = new Gson();
@@ -130,14 +129,33 @@ public class Utility {
 
 
     public static String getErpnextLocation(String locaton) {
-        String serenityLocation = "[{\"locationId\": \"6b46da79-5613-4827-91ae-f46aaf65d4da\",\"locationName\": \"Accra Central (Octagon)\"},"
-        + "{\"locationId\": \"23f59485-8518-4f4e-9146-d061dfe58175\",\"locationName\": \"Airport Primary Care\"},"
-        + "{\"locationId\": \"b60c55f5-63dd-4ba2-9fe9-8192f57aaed2\",\"locationName\": \"Tema Primary Care\"},"
-        + "{\"locationId\": \"a79ae42b-03b7-4f5e-ac1a-cd42729c0b04\",\"locationName\": \"Takoradi Primary Care\"},"
-        + "{\"locationId\": \"29e22113-9d7b-46a6-a857-810ca3567ca7\",\"locationName\": \"Airport Main\"},"
-        + "{\"locationId\": \"25d3f170-2c76-413f-b4e0-7c09b4847b68\",\"locationName\": \"Kumasi\"},"
-        + "{\"locationId\": \"2550dc16-3f64-4cee-b808-6c13b255d159\",\"locationName\": \"Ward - Airport Main\"}"+
-        "]";
+        String serenityLocation = "[\n" +
+        "    {\n" +
+        "        \"location_id\": \"6b46da79-5613-4827-91ae-f46aaf65d4da\",\n" +
+        "        \"location_name\": \"Accra Central (Octagon)\"\n" +
+        "    },\n" +
+        "    {\n" +
+        "        \"location_id\": \"23f59485-8518-4f4e-9146-d061dfe58175\",\n" +
+        "        \"location_name\": \"Airport Primary Care\"\n" +
+        "    },\n" +
+        "    {\n" +
+        "        \"location_id\": \"b60c55f5-63dd-4ba2-9fe9-8192f57aaed2\",\n" +
+        "        \"location_name\": \"Tema Primary Care\"\n" +
+        "    },\n" +
+        "    {\n" +
+        "        \"location_id\": \"a79ae42b-03b7-4f5e-ac1a-cd42729c0b04\",\n" +
+        "        \"location_name\": \"Takoradi Primary Care\"\n" +
+        "    },\n" +
+        "    {\n" +
+        "        \"location_id\": \"29e22113-9d7b-46a6-a857-810ca3567ca7\",\n" +
+        "        \"location_name\": \"Airport Main\"\n" +
+        "    },\n" +
+         "    {\n" +
+        "        \"location_id\": \"2550dc16-3f64-4cee-b808-6c13b255d159\",\n" +
+        "        \"location_name\": \"Ward - Airport Main\"\n" +
+        "    }\n" +
+    "]";
+    
         Gson gson = new Gson();
         Type listType = new TypeToken<ArrayList<SerenityLocation>>() {
         }.getType();
